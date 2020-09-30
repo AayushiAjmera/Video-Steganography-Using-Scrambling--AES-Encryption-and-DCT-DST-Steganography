8bf6c232ede2d81f7d5afd37e3e6cfbda8f67faa
AayushiAjmera <69966210+AayushiAjmera@users.noreply.github.com>
2020-09-30 21:27:31 +0530

    Add files via upload

---
+++ embeddingDST.m
@@ -0,0 +1,141 @@
+clc;
+clear all;
+close all;
+tic;
+vid=VideoReader('C:\Users\aayua\Documents\MATLAB\pexels3.mp4');
+  numFrames = vid.NumberOfFrames;
+  n=numFrames;
+for i = 1:1:n
+  frames = read(vid,i);
+  viddup{i}=frames;
+end 
+covs=viddup{1};
+[x,y,z] = size(covs);
+p=2;
+
+
+while 2.^p<x
+p=p+1;
+end
+p=p-1;
+
+d=2.^p;
+s=d/3;
+s=s*2;
+s=round(s);
+m2=imread('C:\Users\aayua\Documents\MATLAB\messsage1.jpg');
+[a,b,c] = size(m2);
+
+f=0;
+while f*s < b
+    f=f+1; 
+end
+g=(f*s)-b;
+Padd = padarray(m2,[0 g],'post');
+
+e=0;
+while e*s < a
+    e=e+1; 
+end
+w=(e*s)-a;
+
+Padd = padarray(Padd,[w 0],'post');
+[rows, columns, numberOfColorBands] = size(Padd);
+wholeBlockRows = floor(rows / s);
+blockVectorR = [s * ones(1, wholeBlockRows), rem(rows, s)];
+wholeBlockCols = floor(columns / s);
+blockVectorC = [s * ones(1, wholeBlockCols), rem(columns, s)];
+if numberOfColorBands > 1
+  ca = mat2cell(Padd, blockVectorR, blockVectorC, numberOfColorBands);
+else
+  ca = mat2cell(Padd, blockVectorR, blockVectorC);
+end
+ca(e+1,:)=[];
+ca(:,f+1)=[];
+covneed=e*f;
+
+rInteger = randperm(numFrames,covneed);
+[x1,y1,z1] = size(ca{1});
+scrambleOrder = randperm(x1*y1);
+
+
+for ij=1:covneed
+img=viddup{rInteger(ij)};
+[roww, coll, plane]=size(img);
+
+D = dstmtx(d);
+crp=imcrop(img,[0,0,d,d]);
+
+if(plane==3)
+    dctv2(:,:,1)=D*double(crp(:,:,1))*D';
+    dctv2(:,:,2)=D*double(crp(:,:,2))*D';
+    dctv2(:,:,3)=D*double(crp(:,:,3))*D';
+else
+    dctv2=D*crp*D';
+end
+
+sec=double(ca{ij});
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+redChannel = sec(:, :, 1);
+greenChannel = sec(:, :, 2);
+blueChannel = sec(:, :, 3);
+
+redChannel = redChannel(scrambleOrder);
+greenChannel = greenChannel(scrambleOrder);
+blueChannel = blueChannel(scrambleOrder);
+
+redChannel = reshape(redChannel, [x1, y1]);
+greenChannel = reshape(greenChannel, [x1, y1]);
+blueChannel = reshape(blueChannel, [x1, y1]);
+
+randomized_image1 = cat(3, redChannel, greenChannel, blueChannel);
+sec=randomized_image1;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+sec=sec/255;
+for i=(d-s+1):d
+    for j=(d-s+1):d
+        if(plane==3)
+            dctv2(i,j,1)=sec(i-(d-s),j-(d-s),1);
+            dctv2(i,j,2)=sec(i-(d-s),j-(d-s),2);
+            dctv2(i,j,3)=sec(i-(d-s),j-(d-s),3);
+        else
+            dctv2(i,j)=sec(i-(d-s),j-(d-s));
+        end
+        
+    end
+end
+if(plane==3)
+        dctv3(:,:,1)=(D')*double(dctv2(:,:,1))*(D);
+        dctv3(:,:,2)=(D')*double(dctv2(:,:,2))*(D);
+        dctv3(:,:,3)=(D')*double(dctv2(:,:,3))*(D);
+else
+        dctv3=D*dctv2*D';
+end
+temp=viddup{rInteger(ij)};
+temp=double(temp);
+clear i;
+clear j;
+for i=1:d
+    for j=1:d
+                temp(i,j,1)=dctv3(i,j,1);
+                temp(i,j,2)=dctv3(i,j,2);
+                temp(i,j,3)=dctv3(i,j,3);
+    end
+end
+viddup{rInteger(ij)}=uint8(temp);
+end
+
+workingDir = 'C:\Users\aayua\Documents\MATLAB\';
+outputVideo = VideoWriter(fullfile(workingDir,'shuttle_out2'),'Uncompressed AVI');
+outputVideo.FrameRate = 25;
+% outputVideo.LosslessCompression=true;
+
+open(outputVideo)
+for ii = 1:length(viddup)
+   imgi=viddup{ii};
+   writeVideo(outputVideo,imgi)
+end
+close(outputVideo)
+disp("done");
---
+++ extractionSLT.m
@@ -0,0 +1,87 @@
+vid2=VideoReader('C:\Users\aayua\Documents\MATLAB\shuttle_out2.avi');
+  numFrames = vid2.NumberOfFrames;
+  n=numFrames;
+for io=1:covneed
+img2=read(vid2,rInteger(io));
+[roww, coll, plane]=size(img2);
+    
+D = sltmtx(p);
+crp2=imcrop(img2,[0,0,d,d]);
+
+if(plane==3)
+    dctv25(:,:,1)=D*im2double(crp2(:,:,1))*D';
+    dctv25(:,:,2)=D*im2double(crp2(:,:,2))*D';
+    dctv25(:,:,3)=D*im2double(crp2(:,:,3))*D';
+else
+    dctv25=D*crp2*D';
+end
+
+for i=(d-s+1):d
+    for j=(d-s+1):d
+        if(plane==3)
+            sec2(i-(d-s),j-(d-s),1)=dctv25(i,j,1);
+            sec2(i-(d-s),j-(d-s),2)=dctv25(i,j,2);
+            sec2(i-(d-s),j-(d-s),3)=dctv25(i,j,3);
+        else
+            sec2(i-(d-s),j-(d-s))=dctv25(i,j);
+        end
+    end
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ 
+[x1 y1 z1] = size(sec2);
+redChannel = sec2(:, :, 1);
+greenChannel = sec2(:, :, 2);
+blueChannel = sec2(:, :, 3);
+
+recoverOrder = zeros([x1*y1], 2);
+recoverOrder(:, 1) = 1 : (x1*y1);
+recoverOrder(:, 2) = scrambleOrder;
+% Sort this to find out where each scrambled location needs to be sent to.
+newOrder = sortrows(recoverOrder, 2);
+% Extract just column 1, which is the order we need.
+newOrder = newOrder(:,1);
+% Unscramble according to the recoverOrder order.
+redChannel = redChannel(newOrder);
+greenChannel = greenChannel(newOrder);
+blueChannel = blueChannel(newOrder);
+% Reshape into a 2D image
+redChannel = reshape(redChannel, [x1, y1]);
+greenChannel = reshape(greenChannel, [x1, y1]);
+blueChannel = reshape(blueChannel, [x1, y1]);
+
+% Recombine separate color channels into a single, true color RGB image.
+sec2 = cat(3, redChannel, greenChannel, blueChannel);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+rmsg{io}=sec2*255;
+
+end
+
+
+At = cat(1,rmsg{1},rmsg{2});
+At = cat(1,At,rmsg{3});
+At = cat(1,At,rmsg{4});
+Bt = cat(1,rmsg{5},rmsg{6});
+Bt = cat(1,Bt,rmsg{7});
+Bt = cat(1,Bt,rmsg{8});
+Ct = cat(1,rmsg{9},rmsg{10});
+Ct = cat(1,Ct,rmsg{11});
+Ct = cat(1,Ct,rmsg{12});
+Dt = cat(1,rmsg{13},rmsg{14});
+Dt = cat(1,Dt,rmsg{15});
+Dt = cat(1,Dt,rmsg{16});
+Et = cat(1,rmsg{17},rmsg{18});
+Et = cat(1,Et,rmsg{19});
+Et = cat(1,Et,rmsg{20});
+Ft = cat(1,rmsg{21},rmsg{22});
+Ft = cat(1,Ft,rmsg{23});
+Ft = cat(1,Ft,rmsg{24});
+
+catmsg=cat(2,At,Bt);
+catmsg=cat(2,catmsg,Ct);
+catmsg=cat(2,catmsg,Dt);
+catmsg=cat(2,catmsg,Et);
+catmsg=cat(2,catmsg,Ft);
+
+imwrite(catmsg,fullfile(['C:\Users\aayua\Documents\MATLAB\extractedmessage1SLT.jpg']));
+% imwrite(catmsg,fullfile(['C:\Users\aayua\Documents\MATLAB\extractedmessage2.png']));
